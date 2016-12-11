969cadb3f6bccaac132f1c05356136a5d54ea348
tpluscode <tpluscode@users.noreply.github.com>
2016-12-11 16:20:25 +0100

    identifier expander

---
+++ src/Argolis.Models/IUriTemplateExpander.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Argolis.Models
+{
+    /// <summary>
+    /// Used to expand a template by substituting variables with given parameters
+    /// </summary>
+    public interface IUriTemplateExpander
+    {
+        /// <summary>
+        /// Expands template associated with type <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">type of resource model</typeparam>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>A relative or absolute URI depending on the URI template</returns>
+        Uri Expand<T>(IDictionary<string, object> parameters);
+
+        /// <summary>
+        /// Expands template associated with type <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">type of resource model</typeparam>
+        /// <param name="parameters">The parameters anonymous object.</param>
+        /// <returns>A relative or absolute URI depending on the URI template</returns>
+        Uri Expand<T>(object parameters);
+
+        /// <summary>
+        /// Expands template associated with type <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">type of resource model</typeparam>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>A absolute URI identifier</returns>
+        Uri ExpandAbsolute<T>(IDictionary<string, object> parameters);
+
+        /// <summary>
+        /// Expands template associated with type <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">type of resource model</typeparam>
+        /// <param name="parameters">The parameters anonymous object.</param>
+        /// <returns>An absolute URI identifier</returns>
+        Uri ExpandAbsolute<T>(object parameters);
+    }
+}
---
+++ src/Argolis.Models/TunnelVisionLabsUriTemplateExpander.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using DictionaryLibrary;
+using TunnelVisionLabs.Net;
+
+namespace Argolis.Models
+{
+    /// <summary>
+    /// URI Template expander implemented with TunnelVisionLabs.Net.UriTemplate package
+    /// </summary>
+    public class TunnelVisionLabsUriTemplateExpander : IUriTemplateExpander
+    {
+        private readonly IModelTemplateProvider templates;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TunnelVisionLabsUriTemplateExpander"/> class.
+        /// </summary>
+        /// <param name="templates">The template provider.</param>
+        public TunnelVisionLabsUriTemplateExpander(IModelTemplateProvider templates)
+        {
+            this.templates = templates;
+        }
+
+        /// <inheritdoc />
+        public Uri Expand<T>(IDictionary<string, object> parameters)
+        {
+            var templateString = this.templates.GetTemplate(typeof(T));
+            return this.Expand(templateString, parameters);
+        }
+
+        /// <inheritdoc />
+        public Uri Expand<T>(object parameters)
+        {
+            var dictionary = parameters as IDictionary<string, object> ?? DictionaryMaker.Make(parameters);
+            return this.Expand<T>(dictionary);
+        }
+
+        /// <inheritdoc />
+        public Uri ExpandAbsolute<T>(IDictionary<string, object> parameters)
+        {
+            var templateString = this.templates.GetAbsoluteTemplate(typeof(T));
+            return this.Expand(templateString, parameters);
+        }
+
+        /// <inheritdoc />
+        public Uri ExpandAbsolute<T>(object parameters)
+        {
+            var dictionary = parameters as IDictionary<string, object> ?? DictionaryMaker.Make(parameters);
+            return this.ExpandAbsolute<T>(dictionary);
+        }
+
+        private Uri Expand(string templateString, IDictionary<string, object> parameters)
+        {
+            return new UriTemplate(templateString).BindByName(parameters);
+        }
+    }
+}
