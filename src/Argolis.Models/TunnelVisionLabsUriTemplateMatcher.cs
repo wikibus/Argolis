8850e0b09910524354f81f05b302cd154c449d98
tpluscode <tpluscode@users.noreply.github.com>
2016-12-11 14:46:54 +0100

    uri template matcher

---
+++ src/Argolis.Models/TunnelVisionLabsUriTemplateMatcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using TunnelVisionLabs.Net;
+
+namespace Argolis.Models
+{
+    /// <summary>
+    /// Implements <see cref="IUriTemplateMatcher"/> with TunnelVisionLabs.Net.UriTemplate package
+    /// </summary>
+    /// <seealso cref="Argolis.Models.IUriTemplateMatcher" />
+    public class TunnelVisionLabsUriTemplateMatcher : IUriTemplateMatcher
+    {
+        private readonly IModelTemplateProvider templates;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TunnelVisionLabsUriTemplateMatcher"/> class.
+        /// </summary>
+        /// <param name="templates">The template provider.</param>
+        public TunnelVisionLabsUriTemplateMatcher(IModelTemplateProvider templates)
+        {
+            this.templates = templates;
+        }
+
+        /// <inheritdoc/>
+        public UriTemplateMatches Match<T>(Uri uri)
+        {
+            var template = uri.IsAbsoluteUri
+                ? this.templates.GetAbsoluteTemplate(typeof(T))
+                : this.templates.GetTemplate(typeof(T));
+
+            var tunnelVisionMatches = new UriTemplate(template).Match(uri);
+            if (tunnelVisionMatches != null)
+            {
+                var matchDict = tunnelVisionMatches.Bindings.Values.ToDictionary(m => m.Key.Name, m => m.Value);
+                return new UriTemplateMatches(matchDict);
+            }
+
+            return UriTemplateMatches.Failure();
+        }
+    }
+}
---
+++ src/Argolis.Tests/Models/UriTemplateMatchesTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Argolis.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace Argolis.Tests.Models
+{
+    public class UriTemplateMatchesTests
+    {
+        [Fact]
+        public void Failed_match_should_not_throw_on_dictionary_members()
+        {
+            // given
+            var failure = UriTemplateMatches.Failure();
+
+            // then
+            failure.Values.Should().BeEmpty();
+            failure.Keys.Should().BeEmpty();
+            failure.ContainsKey("whatever").Should().BeFalse();
+            object val;
+            failure.TryGetValue("something", out val).Should().BeFalse();
+            failure.Count.Should().Be(0);
+            failure.GetEnumerator().MoveNext().Should().BeFalse();
+        }
+
+        [Fact]
+        public void Successful_match_should_get_converted_values_for_existing_key()
+        {
+            // given
+            var matches = new Dictionary<string, object>
+            {
+                { "key", "5" }
+            };
+            var templateMatches = new UriTemplateMatches(matches);
+
+            // when
+            var actualValue = templateMatches.Get<int>("key");
+
+            // then
+            actualValue.Should().Be(5);
+        }
+
+        [Fact]
+        public void Successful_match_should_get_values_as_is_when_no_conversions_required()
+        {
+            // given
+            var list = new List<int>();
+            var matches = new Dictionary<string, object>
+            {
+                { "key", list }
+            };
+            var templateMatches = new UriTemplateMatches(matches);
+
+            // when
+            var actualValue = templateMatches.Get<List<int>>("key");
+
+            // then
+            actualValue.Should().BeSameAs(list);
+        }
+    }
+}
